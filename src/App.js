e86efe6b0426b7a41d12953d07f93dbb7d7b8365
Tim Williams <timwilliams@fastmail.com>
2018-08-25 20:46:17 -0400

    bare minimum redux boilerplate for redux app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 
 const App = ({}) => (
-  <View style={styles.container}>
-    <Text>I'm App</Text>
-  </View>
+  <Provider store={createStore(reducers)}>
+    <View style={styles.container}>
+      <Text>I'm App</Text>
+    </View>
+  </Provider>
 )
 
 export default App
