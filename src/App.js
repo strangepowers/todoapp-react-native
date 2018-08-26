bb26dc8dba3e0f34024c9c93a7dbab7d6dcb4489
Tim Williams <timwilliams@fastmail.com>
2018-08-25 21:13:08 -0400

    add in header component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import { View, Text, StyleSheet } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import { Header } from './components/common'
 
-const App = ({}) => (
-  <Provider store={createStore(reducers)}>
-    <View style={styles.container}>
-      <Text>I'm App</Text>
-    </View>
-  </Provider>
-)
+const App = ({}) => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
+    </Provider>
+  )
+}
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-    padding: 20,
-    alignSelf: 'center'
-  }
-})
