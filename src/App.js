65468f9e15df6efa5a8b7055f50600875ade469e
Tim Williams <timwilliams@fastmail.com>
2018-09-01 19:12:13 -0400

    simple counter app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { Provider } from 'react-redux'
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Provider, connect } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this._onPressButton2 = this._onPressButton2.bind(this);
+  }
+  _onPressButton(props) {
+    console.log(" ON PRESSSSSSS ", props)
+    this.props.resetNum()
+  }
+  _onPressButton2() {
+    console.log(this.props)
+    console.log(" ON PRESSSSSSS 2")
+    this.props.incrementNum()
+  }
+  render() {
+    console.log("NEW props are ", this.props)
+    return (
+      <View>
+        <Text>The number is {this.props.number} </Text>
+        <TouchableOpacity onPress={() => (this._onPressButton(this.props))}>
+          <Text>Press Me</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this._onPressButton2}>
+          <Text>Press Me 2</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const sTP = ({ count }) => {
+  return {number: count}
+}
+
+const INC_ACTION = {
+  type: "INC_ACTION"
+}
+
+const RESET_ACTION = {
+  type: "RESET"
+}
+
+function mapDispatchToProps(dispatch) {
+    return({
+        incrementNum: () => {dispatch(INC_ACTION)},
+        resetNum: () => {dispatch(RESET_ACTION)}
+    })
+}
+
+let NewComp = connect(sTP, mapDispatchToProps)(MyComponent)
 
 const App = ({}) => (
   <Provider store={createStore(reducers)}>
     <View style={styles.container}>
+      <Text>Counter</Text>
       <Text>I'm App</Text>
+      <NewComp />
     </View>
   </Provider>
 )
+
+//export default App
 
 export default App
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,19 @@
 import { combineReducers } from 'redux'
 
+function numberReducer(state = 12, action) {
+  console.log("state is ", state)
+  console.log("action is ", action)
+  if(action.type === 'INC_ACTION') {
+    return state + 1
+  }
+  if(action.type === "RESET") {
+    return 0
+  }
+  else {
+    return state
+  }
+}
+
 export default combineReducers({
-  libraries: () => []
+  count: numberReducer
 })
